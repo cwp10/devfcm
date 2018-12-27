54b5bea4ce358455f8945f9bf054aead4658f587
Won Pyoung Choi <cwp10@msn.com>
2018-12-27 13:13:38 +0900

    윈격으로 푸쉬 받기

--- app/src/main/java/com/fcmtest/choiwp10/fcmtest/Service/FirebaseInstanceService.java
+++ app/src/main/java/com/fcmtest/choiwp10/fcmtest/Service/FirebaseInstanceService.java
@@ -1,9 +1,19 @@
 package com.fcmtest.choiwp10.fcmtest.Service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.fcmtest.choiwp10.fcmtest.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
 
 public class FirebaseInstanceService extends FirebaseMessagingService {
 
@@ -12,6 +22,8 @@ public class FirebaseInstanceService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
     @Override
@@ -19,6 +31,32 @@ public class FirebaseInstanceService extends FirebaseMessagingService {
         super.onNewToken(s);
 
         Log.d(TAG, s);
+    }
+
+    private void showNotification(String title, String body) {
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "androidfcm.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("TEST Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[] {0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
     }
 }
 
